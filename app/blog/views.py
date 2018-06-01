8652c56b3ef21dfacc9f6e57689669056a341627
pmb <gambler1541@gmail.com>
2018-06-01 16:56:42 +0900

    templates 파일안에 post_list.html 생성
    post_list()를 통해 post_list.html 리턴

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,7 +1,25 @@
+import os
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def post_list(request):
+
+    abs_path = os.path.abspath(__file__)
+    dir_name = os.path.dirname(abs_path)
+    app_dir = os.path.dirname(dir_name)
+    tem_dir = os.path.join(app_dir,'templates')
+    tem_blog = os.path.join(tem_dir, 'blog', 'post_list.html')
+    html = open(tem_blog, 'rt').read()
     return HttpResponse(
-        '<html><body>Post List</body></html>')
+        html
+    )
+
+    # return HttpResponse(
+    #     '<html>'
+    #     '<body>'
+    #     '   <h1>Post List<h1>'
+    #     '   <p>Post list page</p>'
+    #     '   <img src="https://t1.daumcdn.net/cfile/tistory/22334E4F5900DDE029"'
+    #     '</body>'
+    #     '</html>')
