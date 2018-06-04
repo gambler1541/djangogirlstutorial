1353b5abbba3c9dd35d776b0e61c345d2e4dfaf4
pmb <gambler1541@gmail.com>
2018-06-04 12:55:24 +0900

    render_to_string 대신에 render를 사용

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,10 +1,11 @@
 import os
 from django.http import HttpResponse
-from django.template.loader import render_to_string
+from django.shortcuts import render
+
 
 
 def post_list(request):
 
     # 경로에 해당하는 HTMl파일을 문자열로 로드해줌
-    html = render_to_string('blog/post_list.html')
-    return HttpResponse(html)
+    return render(request, 'blog/post_list.html')
+
