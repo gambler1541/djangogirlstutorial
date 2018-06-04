664b7d0e98c89928ea3f7e156198ee79000471d2
pmb <gambler1541@gmail.com>
2018-06-04 15:24:04 +0900

    post_list에서 blog/post_list.html파일을 동적으로 랜더링
    context에서 키값(posts)키로 전달한 QuerySey을 순회하며 div요소를 반복적해서 생성해준다
    
    Merge remote-tracking branch 'origin/master'

--- app/blog/views.py
+++ app/blog/views.py
@@ -6,16 +6,11 @@ from .models import Post
 
 
 def post_list(request):
-
-    # 경로에 해당하는 HTMl파일을 문자열로 로드해줌
-    result = '글 목록<br>'
     posts = Post.objects.all()
-    for post in posts:
-        result += '-{}<br>'.format(post.title)
-
-    return HttpResponse(result)
-
-
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'blog/post_list.html', context)
 
 
 
