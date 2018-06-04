cf306c7fa2da812890b270c4974d81b558b26a3b
pmb <gambler1541@gmail.com>
2018-06-04 15:08:31 +0900

    post_list에서 동적으로 글 목록 문자열을 만들어 리턴해보기

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,11 +1,21 @@
 import os
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 
 def post_list(request):
 
     # 경로에 해당하는 HTMl파일을 문자열로 로드해줌
-    return render(request, 'blog/post_list.html')
+    result = '글 목록<br>'
+    posts = Post.objects.all()
+    for post in posts:
+        result += '-{}<br>'.format(post.title)
+
+    return HttpResponse(result)
+
+
+
+
 
